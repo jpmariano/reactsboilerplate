5f00d75227f7d7bf9bf6e9b2b335d9a3869e189d
zeejay09 <zeejaybelamide@gmail.com>
2020-06-03 16:34:58 +0800

    creates navbar component

---
+++ src/components/NavigationBar/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavLink } from 'react-bootstrap';
+
+class NavBar extends Component {
+
+    render() {
+        return (
+            <header>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <NavLink href={'/'} className="navbar-brand">
+                        Sample
+                    </NavLink>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink href='/about' className="nav-link">About</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink href='/contact' className="nav-link">Contact</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink href='/user' className="nav-link">Login</NavLink>
+                            </li>
+                        </ul>
+                    </Navbar.Collapse>
+                </Navbar>
+            </header>
+        )
+    }
+}
+
+export default NavBar;

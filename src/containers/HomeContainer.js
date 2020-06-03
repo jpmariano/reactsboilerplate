4c3b849a922179819bf56a2ab36beccbf48a0b71
zeejay09 <zeejaybelamide@gmail.com>
2020-06-03 16:35:06 +0800

    creates home container

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+    
+    render() {
+        return (
+            <div className="infobar">
+
+                <h1>Home</h1>
+
+            </div>
+        );
+    }
+}
+
+export default HomeContainer

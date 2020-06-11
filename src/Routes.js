385b6cb4f5ec04ba121da65ddd6dbb304bbbfa4d
zeejay09 <zeejaybelamide@gmail.com>
2020-06-11 17:05:55 +0800

    creates /admin/users route

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import { Route, Switch } from "react-router-dom";
 // My imports
 import Login from './containers/LoginContainer';
 import Home from './containers/HomeContainer';
+import Users from './containers/UserListContainer';
 
 export default function Routes() {
   return (
     <Switch>
         <Route exact path='/' component={ Home } />
         <Route exact path='/login' component={ Login } />
+        <Route exact path='/admin/users' component={ Users } />
     </Switch>
   );
 }

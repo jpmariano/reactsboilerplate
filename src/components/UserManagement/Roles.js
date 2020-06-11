62dbf17b42fe118089a8718b281f9524eae2133d
zeejay09 <zeejaybelamide@gmail.com>
2020-06-11 17:05:42 +0800

    creates User Roles component

---
+++ src/components/UserManagement/Roles.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Roles extends Component {
+    
+    render() {
+        return (
+            <div className="container-fluid">
+
+                <h1>Roles</h1>
+
+            </div>
+        );
+    }
+}
+
+export default Roles

7baedf74cac577d39a621b52617c1076c44793b3
zeejay09 <zeejaybelamide@gmail.com>
2020-06-11 17:13:58 +0800

    adds initial table and add user button. Still work in progress

--- src/components/UserManagement/List.js
+++ src/components/UserManagement/List.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Table from 'react-bootstrap/Table';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 class List extends Component {
     
@@ -6,7 +9,50 @@ class List extends Component {
         return (
             <div className="container-fluid">
 
-                <h1>List</h1>
+                <button className="btn btn-primary mt-3 mr-3 mb-3"><FontAwesomeIcon icon={faPlus}/> Add User</button>
+
+                <Table striped bordered hover responsive size="sm">
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>Table heading</th>
+                        <th>Table heading</th>
+                        <th>Table heading</th>
+                        <th>Table heading</th>
+                        <th>Table heading</th>
+                        <th>Table heading</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>1</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        </tr>
+                        <tr>
+                        <td>2</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        </tr>
+                        <tr>
+                        <td>3</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        </tr>
+                    </tbody>
+                </Table>
 
             </div>
         );

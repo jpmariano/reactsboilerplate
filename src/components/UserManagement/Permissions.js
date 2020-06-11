9599f18482d15b892fcf5d5d50a6cbbb72f1983b
zeejay09 <zeejaybelamide@gmail.com>
2020-06-11 17:05:35 +0800

    creates User Permissions component

---
+++ src/components/UserManagement/Permissions.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Permissions extends Component {
+    
+    render() {
+        return (
+            <div className="container-fluid">
+
+                <h1>Permissions</h1>
+
+            </div>
+        );
+    }
+}
+
+export default Permissions

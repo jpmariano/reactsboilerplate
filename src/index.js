718f4058ff05e8c095f1d915547ab8cb986e38a5
zeejay09 <zeejaybelamide@gmail.com>
2020-06-11 12:15:11 +0800

    adds a baseURL for axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+// local Server
+axios.defaults.baseURL = 'http://localhost:8080/v1';
 
 ReactDOM.render(
   <React.StrictMode>

5b0ddcc2dd85838d3581181ac029d8efc2777074
zeejay09 <zeejaybelamide@gmail.com>
2020-06-12 11:49:58 +0800

    removes the this context

--- src/components/UserManagement/List.js
+++ src/components/UserManagement/List.js
@@ -30,7 +30,7 @@ function List() {
     return (
         <div className="container-fluid">
 
-            <button className="btn btn-primary mt-3 mr-3 mb-3" onClick={() => this.setModalShow(true)}><FontAwesomeIcon icon={faPlus}/> Add User</button>
+            <button className="btn btn-primary mt-3 mr-3 mb-3" onClick={() => setModalShow(true)}><FontAwesomeIcon icon={faPlus}/> Add User</button>
 
             <Table striped bordered hover responsive size="sm">
                 <thead>

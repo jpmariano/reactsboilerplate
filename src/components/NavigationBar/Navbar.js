ba0c942bd9c0c642476bb83d008a4532e322a9e9
zeejay09 <zeejaybelamide@gmail.com>
2020-06-12 09:24:27 +0800

    modifies class-like style to react hooks style of coding

--- src/components/NavigationBar/Navbar.js
+++ src/components/NavigationBar/Navbar.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavLink } from 'react-bootstrap';
 
-class NavBar extends Component {
+function NavBar() {
 
-    render() {
-        return (
-            <header>
-                <Navbar bg="dark" variant="dark" expand="lg">
-                    <NavLink href={'/'} className="navbar-brand">
-                        Sample
-                    </NavLink>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <NavLink href='/about' className="nav-link">About</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink href='/contact' className="nav-link">Contact</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink href='/login' className="nav-link">Login</NavLink>
-                            </li>
-                        </ul>
-                    </Navbar.Collapse>
-                </Navbar>
-            </header>
-        )
-    }
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <NavLink href={'/'} className="navbar-brand">
+                    Sample
+                </NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink href='/about' className="nav-link">About</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink href='/contact' className="nav-link">Contact</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink href='/login' className="nav-link">Login</NavLink>
+                        </li>
+                    </ul>
+                </Navbar.Collapse>
+            </Navbar>
+        </header>
+    )
 }
 
 export default NavBar;

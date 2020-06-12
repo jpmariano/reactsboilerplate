6b528c803482cc1b9c4234333b34e56168f713cc
zeejay09 <zeejaybelamide@gmail.com>
2020-06-12 09:24:48 +0800

    adjusts the imported component to the latest changes

--- src/components/UserManagement/List.js
+++ src/components/UserManagement/List.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 // components
-import AddUserForm from './AddUserForm';
+import AddUserForm from './UserForm';
 
 class List extends Component {
     

7685928d0271d16e74b4175c24a8923912fe3bd3
zeejay09 <zeejaybelamide@gmail.com>
2020-06-12 09:44:45 +0800

    adds condition to protect authenticated routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 // My imports
 import Login from './containers/LoginContainer';
@@ -7,11 +7,22 @@ import Home from './containers/HomeContainer';
 import Users from './containers/UserListContainer';
 
 export default function Routes() {
-  return (
-    <Switch>
-        <Route exact path='/' component={ Home } />
-        <Route exact path='/login' component={ Login } />
-        <Route exact path='/admin/users' component={ Users } />
-    </Switch>
-  );
+
+    const [isLoggedIn] = useState(sessionStorage.getItem('isLoggedIn') ? sessionStorage.getItem('isLoggedIn') : false);
+
+    return (
+        <Switch>
+            {
+                isLoggedIn ?
+                    <>
+                        <Route exact path='/admin/users' component={ Users } />
+                    </>
+                :  
+                    <>
+                        <Route exact path='/login' component={ Login } />
+                        <Route exact path='/' component={ Home } />
+                    </>
+            }
+        </Switch>
+    );
 }

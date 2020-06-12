f563a26ab40c9e8c513d7a8ccd530fa694efaa7b
zeejay09 <zeejaybelamide@gmail.com>
2020-06-12 11:23:35 +0800

    removes the protection for the authenticated path, I just put each page a protection, I will improve this in the future

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 // My imports
@@ -8,21 +8,11 @@ import Users from './containers/UserListContainer';
 
 export default function Routes() {
 
-    const [isLoggedIn] = useState(sessionStorage.getItem('isLoggedIn') ? sessionStorage.getItem('isLoggedIn') : false);
-
     return (
         <Switch>
-            {
-                isLoggedIn ?
-                    <>
-                        <Route exact path='/admin/users' component={ Users } />
-                    </>
-                :  
-                    <>
-                        <Route exact path='/login' component={ Login } />
-                        <Route exact path='/' component={ Home } />
-                    </>
-            }
+            <Route exact path='/admin/users' component={ Users } />
+            <Route exact path='/login' component={ Login } />
+            <Route exact path='/' component={ Home } />
         </Switch>
     );
 }

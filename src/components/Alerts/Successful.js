f46e70fd807d38bb48a79636cdadf3f60eafc133
zeejay09 <zeejaybelamide@gmail.com>
2020-06-16 18:43:10 +0800

    modifies the SuccessModal to accommodate the redirect to login if the component is used by the registration page

--- src/components/Alerts/Successful.js
+++ src/components/Alerts/Successful.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Button } from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 
 function Successful(props) {
 
     const [modalShow, setModalShow] = useState(props.modalShow);
+    const [isRegistered, setIsRegistered] = useState(false);
 
     useEffect(() => {
         setModalShow(props.modalShow);
     }, [props.modalShow]);
+
+    if (props.location === 'register') {
+        if (isRegistered) {
+            return <Redirect to="/login" />;
+        }
+    }
 
     const successMsg = (
         <Modal
@@ -21,10 +29,15 @@ function Successful(props) {
                 <Modal.Title>Success</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                User successfully added!
+                {props.modalMessage}
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="primary" onClick={() => setModalShow(false)}>Okay</Button>
+                {
+                    props.location === 'register' ?
+                        <Button variant="primary" onClick={() => {setModalShow(false); setIsRegistered(true);}}>Okay</Button>
+                    :
+                        <Button variant="primary" onClick={() => setModalShow(false)}>Okay</Button>
+                }
             </Modal.Footer>
         </Modal>
     );

59cf93e0feeea0a39c0dc42950ac474ab39a2db5
zeejay09 <zeejaybelamide@gmail.com>
2020-06-16 12:03:04 +0800

    makes the user list work, it will now display the list of users fetched from the API

--- src/components/UserManagement/List.js
+++ src/components/UserManagement/List.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 // components
 import AddUserForm from './UserForm';
@@ -10,6 +11,7 @@ import SuccessModal from '../Alerts/Successful';
 function List() {
     
     const [modalShow, setModalShow] = useState(false);
+    const [users, setUsersData] = useState([]);
 
     const addUserModal = (
         <Modal
@@ -28,12 +30,31 @@ function List() {
         </Modal>
     );
 
+    async function fetchData() {
+        const result = await axios.get('/users?pageNo=0&pageSize=10&sortBy=uid',);
+
+        setUsersData(result.data);
+        console.log(result.data);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
         <div className="container-fluid">
 
             <button className="btn btn-primary mt-3 mr-3 mb-3" onClick={() => setModalShow(true)}><FontAwesomeIcon icon={faPlus}/> Add User</button>
 
-            
+            <ul>
+                {
+                    users.map(item => (
+                        <li key={item.uid}>
+                            {item.name}
+                        </li>
+                    ))
+                }
+            </ul>
             {addUserModal}
             <SuccessModal modalShow={modalShow} />
         </div>

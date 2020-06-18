0103989b05248ec338e34d29d947a698d8f5274f
zeejay09 <zeejaybelamide@gmail.com>
2020-06-18 14:47:32 +0800

    Modifies routes to accommodate the new PrivateRoute component

--- src/Routes.js
+++ src/Routes.js
@@ -7,16 +7,20 @@ import Home from './containers/HomeContainer';
 import Dashboard from './containers/UserListContainer';
 import Register from './containers/RegisterContainer';
 import Logout from './components/Authentication/Logout';
+import { PrivateRoute } from './components/Routes';
 
 export default function Routes() {
 
     return (
         <Switch>
-            <Route exact path='/admin/users' component={ Dashboard } />
+            {/* Private Routes */}
+            <PrivateRoute exact path='/admin/users' component={ Dashboard } />
+            <PrivateRoute exact path='/logout' component={ Logout } />
+
+            {/* Public Routes */}
             <Route exact path='/' component={ Home } />
             <Route exact path='/login' component={ Login } />
             <Route exact path='/register' component={ Register } />
-            <Route exact path='/logout' component={ Logout } />
         </Switch>
     );
 }

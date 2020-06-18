34fd564a30c681bebd36d3ed49462f5ba20b0423
zeejay09 <zeejaybelamide@gmail.com>
2020-06-18 13:38:11 +0800

    applies redux to the app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
+import { Provider } from 'react-redux';
+import { store } from './helpers';
+
+// styles
+import './index.css';
 import './styles/css/styles.css';
 import './styles/bootstrap/css/bootstrap.min.css';
 
-// local Server
-axios.defaults.baseURL = 'http://localhost:8080/v1';
-
 ReactDOM.render(
 //   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 //   </React.StrictMode>,
   ,document.getElementById('root')
 );

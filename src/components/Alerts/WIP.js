b60ec22131a5d961e4ea1ffdf84eccdb0e912846
zeejay09 <zeejaybelamide@gmail.com>
2020-06-19 21:03:39 +0800

    adds a WIP modal

---
+++ src/components/Alerts/WIP.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+function WIP(props) {
+    
+    const [modalShow, setModalShow] = useState(props.wipModal);
+
+    useEffect(() => {
+        setModalShow(props.wipModal);
+    }, [props.wipModal]);
+
+    console.log(props.wipModal);
+
+    const wipMsg = (
+        <Modal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            size="md"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Work in progress</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {props.modalMessage}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button 
+                    variant="primary" 
+                    onClick={() => {
+                            setModalShow(false);
+                        }
+                    }
+                >
+                    Okay
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+
+    return (
+        <>
+            {wipMsg}
+        </>
+    );
+}
+
+export default WIP;

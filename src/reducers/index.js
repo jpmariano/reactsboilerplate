444cb7c0e03a6f7a7b4932d53de5347f733b5ebc
zeejay09 <zeejaybelamide@gmail.com>
2020-06-22 19:01:51 +0800

    adds role reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import { authentication } from './authentication.reducer';
 import { registration } from './registration.reducer';
 import { users } from './users.reducer';
 import { alert } from './alert.reducer';
+import { role } from './role.reducer';
 
 const rootReducer = combineReducers({
   authentication,
   registration,
   users,
-  alert
+  alert,
+  role
 });
 
 export default rootReducer;

9795009ad07e08b4e992d52ea5d7be4dee641a64
zeejay09 <zeejaybelamide@gmail.com>
2020-06-23 18:27:15 +0800

    uses formik and prepare the list for value changing and updating

--- src/components/List/Permissions.js
+++ src/components/List/Permissions.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Formik } from "formik";
 
 // material ui
 import Paper from '@material-ui/core/Paper';
@@ -8,7 +9,6 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Checkbox from '@material-ui/core/Checkbox';
 
@@ -18,45 +18,47 @@ import { permissionAction } from '../../actions';
 // components
 import AppStyles from '../Common/useStyles';
 
-const columns = [
+const permissionsColumn = [
     { 
         id: 'name', 
         label: 'Permissions', 
         minWidth: 750 
     },
+];
+
+const rolesColumn = [
     { 
-        id: 'role_authenticated', 
+        id: 3, 
         label: 'ROLE_AUTHENTICATED', 
         mWidth: 50,
         align: 'center',
         format: (value) => value === 3 ? true : false,
     },
     {
-        id: 'role_main',
+        id: 2,
         label: 'ROLE_MAIN',
         minWidth: 50,
         align: 'center',
         format: (value) => value === 2 ? true : false,
     },
     {
-        id: 'role_admin',
+        id: 1,
         label: 'ROLE_ADMIN',
         minWidth: 50,
         align: 'center',
         format: (value) => value === 1 ? true : false,
     },
-];
+]
 
 function Permissions() {
    
     // table-related variables
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
     const classes = AppStyles();
 
     // permission-related variables
     const permissionsList = useSelector(state => state.permissions.items);
     const permissions = permissionsList ? permissionsList : [];
+    const [roleWrite, setRoleWrite] = permissionsList ? permissionsList : [];
 
     const dispatch = useDispatch();
 
@@ -69,69 +71,183 @@ function Permissions() {
         fetchData();
     }, [dispatch]);
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-
     return (
         <div className="list-container">
+            <Formik
+                initialValues={{
+                    // name: user ? user.name : '',
+                    // username: user ? user.username : '',
+                    // password: '',
+                    // status: user ? user.status : 0,
+                }}
 
-            <Paper className="w-100 border">
-                <TableContainer className={classes.container}>
-                    <Table stickyHeader size="small" aria-label="sticky table">
-                        <TableHead>
-                            <TableRow>
-                            {columns.map((column) => (
-                                <TableCell
-                                    key={column.label}
-                                    align={column.align}
-                                    style={{ minWidth: column.minWidth }}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {permissions.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((permission, index) => {
-                                return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={index}>
-                                        {
-                                            columns.map((column) => {
-                                                const value = permission[column.id];
+                onSubmit={async values => {
+                    console.log(values)
+                }}
+            >
+                
+                {props => {
+                    const {
+                        values,
+                        isSubmitting,
+                        handleChange,
+                        handleSubmit,
+                        setFieldValue
+                } = props;
+                    return (
+                        <form onSubmit={handleSubmit}>
+                            <Paper className="w-100 border">
+                                <TableContainer className={classes.container}>
+                                    <Table stickyHeader size="small" aria-label="sticky table">
+                                        <TableHead>
+                                            <TableRow>
+                                            {permissionsColumn.map((column) => (
+                                                <TableCell
+                                                    key={column.label}
+                                                    align={column.align}
+                                                    style={{ minWidth: column.minWidth }}
+                                                >
+                                                    {column.label}
+                                                </TableCell>
+                                            ))}
+                                            {rolesColumn.map((column) => (
+                                                <TableCell
+                                                    key={column.label}
+                                                    align={column.align}
+                                                    style={{ minWidth: column.minWidth }}
+                                                >
+                                                    {column.label}
+                                                </TableCell>
+                                            ))}
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {permissions.map((permission, index) => {
                                                 return (
-                                                    <TableCell key={column.label} align={column.align}>
+                                                    <TableRow hover role="checkbox" tabIndex={-1} key={index}>
                                                         {
-                                                            (column.id !== "role_admin") && (column.id !== "role_main") && (column.id !== "role_authenticated") ?
-                                                                column.format && typeof value === 'number' ? column.format(value) : value
-                                                            :
-                                                                <Checkbox key={index} color="primary" checked={false}/>
+                                                            permissionsColumn.map((column) => {
+                                                                const value = permission[column.id];
+                                                                return (
+                                                                    <TableCell key={column.label} align={column.align}>
+                                                                        {
+                                                                            column.format && typeof value === 'number' ? column.format(value) : value
+                                                                        }
+                                                                    </TableCell>
+                                                                );
+                                                            })
                                                         }
-                                                    </TableCell>
+                                                        {
+                                                            rolesColumn.map((column, index) => {
+                                                                return (
+                                                                    column.id === 3 ?
+                                                                        <TableCell key={column.id} align={column.align}>
+                                                                            {
+                                                                                permission.roles.includes(column.id) ? 
+                                                                                    <Checkbox
+                                                                                        key={index}
+                                                                                        color="primary"
+                                                                                        checked={column.format(column.id)}
+                                                                                        onChange={() => {
+                                                                                                console.log(permission.pid); console.log(column.id)
+                                                                                            }
+                                                                                        }
+                                                                                    />
+                                                                                :
+                                                                                    <Checkbox
+                                                                                        key={index}
+                                                                                        color="primary"
+                                                                                        checked={false}
+                                                                                        onChange={() => {
+                                                                                                console.log(permission.pid); console.log(column.id)
+                                                                                            }
+                                                                                        }
+                                                                                    />
+                                                                            }
+                                                                        </TableCell>
+                                                                    :
+                                                                        null
+                                                                );
+                                                            })
+                                                        }
+                                                        {
+                                                            rolesColumn.map((column, index) => {
+                                                                return (
+                                                                    column.id === 2 ?
+                                                                        <TableCell key={column.id} align={column.align}>
+                                                                            {
+                                                                                permission.roles.includes(column.id) ? 
+                                                                                    <Checkbox
+                                                                                        key={index}
+                                                                                        color="primary"
+                                                                                        checked={column.format(column.id)}
+                                                                                        onChange={() => {
+                                                                                                console.log(permission.pid); console.log(column.id)
+                                                                                            }
+                                                                                        }
+                                                                                    />
+                                                                                :
+                                                                                    <Checkbox
+                                                                                        key={index}
+                                                                                        color="primary"
+                                                                                        checked={false}
+                                                                                        onChange={() => {
+                                                                                                console.log(permission.pid); console.log(column.id)
+                                                                                            }
+                                                                                        }
+                                                                                    />
+                                                                            }
+                                                                        </TableCell>
+                                                                    :
+                                                                        null
+                                                                );
+                                                            })
+                                                        }
+                                                        {
+                                                            rolesColumn.map((column, index) => {
+                                                                return (
+                                                                    column.id === 1 ?
+                                                                        <TableCell key={column.id} align={column.align}>
+                                                                            {
+                                                                                permission.roles.includes(column.id) ? 
+                                                                                    <Checkbox
+                                                                                        key={index}
+                                                                                        color="primary"
+                                                                                        checked={column.format(column.id)}
+                                                                                        onChange={() => {
+                                                                                                console.log(permission.pid); console.log(column.id)
+                                                                                            }
+                                                                                        }/>
+                                                                                :
+                                                                                    <Checkbox
+                                                                                        key={index}
+                                                                                        color="primary"
+                                                                                        checked={false}
+                                                                                        onChange={() => {
+                                                                                                console.log(permission.pid); console.log(column.id)
+                                                                                            }
+                                                                                        }/>
+                                                                            }
+                                                                        </TableCell>
+                                                                    :
+                                                                        null
+                                                                );
+                                                            })
+                                                        }
+                                                    </TableRow>
                                                 );
-                                            })
-                                        }
-                                    </TableRow>
-                                );
-                            })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component="div"
-                    count={permissions.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onChangePage={handleChangePage}
-                    onChangeRowsPerPage={handleChangeRowsPerPage}
-                />
-            </Paper>
+                                            })}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Paper>
+                            <button type="submit" className="btn btn-primary mt-3" disabled={isSubmitting}>
+                                Save Permissions
+                            </button>
+                        </form>
+                    );
+                }}
+            </Formik>
         </div>
     );
 }

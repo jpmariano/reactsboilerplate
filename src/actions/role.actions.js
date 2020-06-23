287e8c11d3af792d57804d6fd03f46f126f44cdf
zeejay09 <zeejaybelamide@gmail.com>
2020-06-23 19:14:39 +0800

    adds role update object

--- src/actions/role.actions.js
+++ src/actions/role.actions.js
@@ -4,6 +4,7 @@ import { alertActions } from './';
 
 export const roleActions = {
     getRole,
+    updateRole
 };
 
 function getRole(id) {
@@ -11,6 +12,27 @@ function getRole(id) {
         dispatch(request(id));
 
         roleService.getRole(id)
+            .then(
+                role => { 
+                    dispatch(success(role));
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(alertActions.error(error.toString()));
+                }
+            );
+    };
+
+    function request(role) { return { type: roleConstants.ROLE_REQUEST, role } }
+    function success(role) { return { type: roleConstants.ROLE_SUCCESS, role } }
+    function failure(error) { return { type: roleConstants.ROLE_FAILURE, error } }
+}
+
+function updateRole(object, id) {
+    return dispatch => {
+        dispatch(request(id));
+
+        roleService.updateRole(object, id)
             .then(
                 role => { 
                     dispatch(success(role));

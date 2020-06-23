e14ea3322a1a1d69a218c69b03f950b7f157e25b
zeejay09 <zeejaybelamide@gmail.com>
2020-06-23 14:27:42 +0800

    adds a permission nav item

--- src/components/Navigation/NavItems.js
+++ src/components/Navigation/NavItems.js
@@ -8,5 +8,10 @@ export default [
         name: 'Users',
         url: '/admin/users',
         icon: 'people_alt'
+    },
+    {
+        name: 'Permissions',
+        url: '/admin/users/permissions',
+        icon: 'vpn_key'
     }
 ];

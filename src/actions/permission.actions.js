fad04a576f489aa7919fbd797d6a6b081e7d7413
zeejay09 <zeejaybelamide@gmail.com>
2020-06-25 14:48:24 +0800

    adds addPermission action

--- src/actions/permission.actions.js
+++ src/actions/permission.actions.js
@@ -1,8 +1,11 @@
 import { permissionConstants } from '../constants';
 import { permissionService } from '../services';
+import { alertActions } from './';
+import { history } from '../helpers';
 
-export const permissionAction = {
+export const permissionActions = {
     getAll,
+    addPermission,
 };
 
 function getAll() {
@@ -19,4 +22,27 @@ function getAll() {
     function request() { return { type: permissionConstants.GETALL_REQUEST } }
     function success(permissions) { return { type: permissionConstants.GETALL_SUCCESS, permissions } }
     function failure(error) { return { type: permissionConstants.GETALL_FAILURE, error } }
+}
+
+function addPermission(permission) {
+    return dispatch => {
+        dispatch(request(permission));
+
+        permissionService.addPermission(permission)
+            .then(
+                permission => { 
+                    dispatch(success(permission));
+                    history.push('/admin/users/permissions');
+                    dispatch(alertActions.success('Registration successful'));
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(alertActions.error(error.toString()));
+                }
+            );
+    };
+
+    function request(permission) { return { type: permissionConstants.ADD_REQUEST, permission } }
+    function success(permission) { return { type: permissionConstants.ADD_SUCCESS, permission } }
+    function failure(error) { return { type: permissionConstants.ADD_FAILURE, error } }
 }

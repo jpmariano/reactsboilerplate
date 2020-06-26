6d9e32d5512f2c89e526a84830ca87540554ffac
zeejay09 <zeejaybelamide@gmail.com>
2020-06-26 11:29:35 +0800

    implements the new customize tabbing deisgn in roles page

--- src/containers/RoleListContainer.js
+++ src/containers/RoleListContainer.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers, faKey } from '@fortawesome/free-solid-svg-icons';
+
+// material ui
+import DeviceHubIcon from '@material-ui/icons/DeviceHub';
 
 // components
 import Roles from '../components/List/Roles';
@@ -14,9 +19,38 @@ function RoleListContainer() {
     }
 
     return (
-        <div id="role-container">
+        <div id="container">
             <h1>Roles</h1>
-            <Roles />
+            <div className="tabs">
+                <div className="content border rounded p-3">
+                    <ul className="col-md-5 mt-3 mb-0">
+                        <li title="Users" className="btn btn-light inactive mr-2">
+                            <label for="tab1" role="button">
+                                <FontAwesomeIcon icon={faUsers}/>&nbsp;
+                                <span>Users</span>
+                            </label>
+                            <div class="indicator"></div>
+                        </li>
+                        <li title="Permissions" className="btn btn-light inactive mr-2">
+                            <label for="tab2" role="button">
+                                <FontAwesomeIcon icon={faKey}/>&nbsp;
+                                <span>Permissions</span>
+                            </label>
+                            <div class="indicator"></div>
+                        </li>
+                        <li title="Roles" className="btn btn-primary active mr-2">
+                            <label for="tab3" role="button">
+                                <DeviceHubIcon />&nbsp;
+                                <span>Roles</span>
+                            </label>
+                            <div class="indicator"></div>
+                        </li>
+                    </ul>
+                    <section>
+                        <Roles />
+                    </section>
+                </div>  
+            </div>
         </div>
     );
 }

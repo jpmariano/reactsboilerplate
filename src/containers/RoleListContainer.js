d64aa48cff57c5011e202d5c03719c2acecc050e
zeejay09 <zeejaybelamide@gmail.com>
2020-06-26 11:36:23 +0800

    adds links to each customize tab header/button items

--- src/containers/RoleListContainer.js
+++ src/containers/RoleListContainer.js
@@ -25,25 +25,31 @@ function RoleListContainer() {
                 <div className="content border rounded p-3">
                     <ul className="col-md-5 mt-3 mb-0">
                         <li title="Users" className="btn btn-light inactive mr-2">
-                            <label for="tab1" role="button">
-                                <FontAwesomeIcon icon={faUsers}/>&nbsp;
-                                <span>Users</span>
-                            </label>
-                            <div class="indicator"></div>
+                            <a href="/admin/users">
+                                <label for="tab1" role="button">
+                                    <FontAwesomeIcon icon={faUsers}/>&nbsp;
+                                    <span>Users</span>
+                                </label>
+                                <div class="indicator"></div>
+                            </a>
                         </li>
                         <li title="Permissions" className="btn btn-light inactive mr-2">
-                            <label for="tab2" role="button">
-                                <FontAwesomeIcon icon={faKey}/>&nbsp;
-                                <span>Permissions</span>
-                            </label>
-                            <div class="indicator"></div>
+                            <a href="/admin/users/permissions">
+                                <label for="tab2" role="button">
+                                    <FontAwesomeIcon icon={faKey}/>&nbsp;
+                                    <span>Permissions</span>
+                                </label>
+                                <div class="indicator"></div>
+                            </a>
                         </li>
                         <li title="Roles" className="btn btn-primary active mr-2">
-                            <label for="tab3" role="button">
-                                <DeviceHubIcon />&nbsp;
-                                <span>Roles</span>
-                            </label>
-                            <div class="indicator"></div>
+                            <a href="/admin/users/roles">
+                                <label for="tab3" role="button">
+                                    <DeviceHubIcon />&nbsp;
+                                    <span>Roles</span>
+                                </label>
+                                <div class="indicator"></div>
+                            </a>
                         </li>
                     </ul>
                     <section>

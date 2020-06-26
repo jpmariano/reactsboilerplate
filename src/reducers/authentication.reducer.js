08c6978c2a50948ed13a86d050d4fd88807d5c96
zeejay09 <zeejaybelamide@gmail.com>
2020-06-26 19:35:07 +0800

    adds getById constant switch cases in the reducer

--- src/reducers/authentication.reducer.js
+++ src/reducers/authentication.reducer.js
@@ -6,20 +6,35 @@ const initialState = user ? { loggedIn: true, user } : {};
 export function authentication(state = initialState, action) {
   switch (action.type) {
     case userConstants.LOGIN_REQUEST:
-      return {
-        loggingIn: true,
-        user: action.user
-      };
+        return {
+            loggingIn: true,
+            user: action.user
+        };
     case userConstants.LOGIN_SUCCESS:
-      return {
-        loggedIn: true,
-        user: action.user
-      };
+        return {
+            loggedIn: true,
+            user: action.user
+        };
     case userConstants.LOGIN_FAILURE:
-      return {};
+        return {};
     case userConstants.LOGOUT:
-      return {};
+        return {};
+    case userConstants.GETBYID_REQUEST:
+        return {
+            ...state,
+            retrieving: true
+        };
+    case userConstants.GETBYID_SUCCESS:
+
+        const { retrieving, ...stateCopy } = state;
+        return {
+            ...stateCopy,
+            retrieved: true,
+            user: action.user
+        };
+    case userConstants.GETBYID_FAILURE:
+        return {};
     default:
-      return state
+        return state
   }
 }

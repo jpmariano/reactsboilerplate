f2fa541fb44f400cdd4d55051f4e783f05d56ad4
zeejay09 <zeejaybelamide@gmail.com>
2020-06-29 17:34:17 +0800

    adds password reset alert actions

--- src/actions/alert.actions.js
+++ src/actions/alert.actions.js
@@ -3,7 +3,9 @@ import { alertConstants } from '../constants';
 export const alertActions = {
     success,
     error,
-    clear
+    clear,
+    passwordResetSuccess,
+    passwordResetError
 };
 
 function success(message) {
@@ -12,6 +14,14 @@ function success(message) {
 
 function error(message) {
     return { type: alertConstants.ERROR, message };
+}
+
+function passwordResetSuccess(message) {
+    return { type: alertConstants.PASSWORD_RESET_SUCCESS, message };
+}
+
+function passwordResetError(message) {
+    return { type: alertConstants.PASSWORD_RESET_ERROR, message };
 }
 
 function clear() {

1cee72a3ff4512e174359a303a686b2e1540b01e
zeejay09 <zeejaybelamide@gmail.com>
2020-06-30 19:58:51 +0800

    captures the vkey param in the URL

--- src/containers/PasswordResetContainer.js
+++ src/containers/PasswordResetContainer.js
@@ -4,9 +4,11 @@ import React from 'react';
 import PasswordResetPage from '../components/Password/Reset';
 import NewPasswordPage from '../components/Password/New';
 
-function PasswordResetContainer() {
+function PasswordResetContainer(props) {
 
     const currUrl = window.location.pathname;
+    const { match } = props;
+    const vkey = match.params.vkey;
 
     return (
         <>
@@ -18,10 +20,10 @@ function PasswordResetContainer() {
                         </div>
                     </div>
                 :
-                    currUrl === '/new-password' &&
+                    vkey &&
                         <div className="new-password-container">
                             <div className="content p-3">
-                                <NewPasswordPage />
+                                <NewPasswordPage vkey={vkey}/>
                             </div>
                         </div>
             }

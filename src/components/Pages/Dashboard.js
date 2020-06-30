699be0ea99f6aebb242f5a9a343ea3aba33b995e
zeejay09 <zeejaybelamide@gmail.com>
2020-06-30 15:11:34 +0800

    adds line graph in the cards with sample data

--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// chartjs
+import { Line } from 'react-chartjs-2';
+
 // material ui
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -25,9 +28,45 @@ const useStyles = makeStyles({
     },
 });
 
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+};
+
+const options = {
+    legend: {
+        display: false,
+    },
+    scales: { 
+        xAxes: [{ display: false, }], 
+        yAxes: [{ display: false, }], 
+    },
+};
+
 function Dashboard() {
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
 
     return (
         <div className="row">
@@ -37,17 +76,10 @@ function Dashboard() {
                         <Typography className={classes.title} color="textSecondary" gutterBottom>
                             Card 1
                         </Typography>
-                        <Typography variant="h5" component="h2">
-                            be{bull}nev{bull}o{bull}lent
-                        </Typography>
-                        <Typography className={classes.pos} color="textSecondary">
-                            adjective
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            well meaning and kindly.
-                            <br />
-                            {'"a benevolent smile"'}
-                        </Typography>
+                        <Line
+                            data={data}
+                            options={options}
+                        />
                     </CardContent>
                     <CardActions>
                         <Button size="small">Learn More</Button>
@@ -60,17 +92,10 @@ function Dashboard() {
                         <Typography className={classes.title} color="textSecondary" gutterBottom>
                             Card 2
                         </Typography>
-                        <Typography variant="h5" component="h2">
-                            be{bull}nev{bull}o{bull}lent
-                        </Typography>
-                        <Typography className={classes.pos} color="textSecondary">
-                            adjective
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            well meaning and kindly.
-                            <br />
-                            {'"a benevolent smile"'}
-                        </Typography>
+                        <Line
+                            data={data}
+                            options={options}
+                        />
                     </CardContent>
                     <CardActions>
                         <Button size="small">Learn More</Button>
@@ -83,17 +108,10 @@ function Dashboard() {
                         <Typography className={classes.title} color="textSecondary" gutterBottom>
                             Card 3
                         </Typography>
-                        <Typography variant="h5" component="h2">
-                            be{bull}nev{bull}o{bull}lent
-                        </Typography>
-                        <Typography className={classes.pos} color="textSecondary">
-                            adjective
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            well meaning and kindly.
-                            <br />
-                            {'"a benevolent smile"'}
-                        </Typography>
+                        <Line
+                            data={data}
+                            options={options}
+                        />
                     </CardContent>
                     <CardActions>
                         <Button size="small">Learn More</Button>
@@ -106,17 +124,10 @@ function Dashboard() {
                         <Typography className={classes.title} color="textSecondary" gutterBottom>
                             Card 4
                         </Typography>
-                        <Typography variant="h5" component="h2">
-                            be{bull}nev{bull}o{bull}lent
-                        </Typography>
-                        <Typography className={classes.pos} color="textSecondary">
-                            adjective
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            well meaning and kindly.
-                            <br />
-                            {'"a benevolent smile"'}
-                        </Typography>
+                        <Line
+                            data={data}
+                            options={options}
+                        />
                     </CardContent>
                     <CardActions>
                         <Button size="small">Learn More</Button>

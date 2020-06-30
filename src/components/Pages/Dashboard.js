41ec91accda91f2ea772a6c2c023606a7fb96087
zeejay09 <zeejaybelamide@gmail.com>
2020-06-30 15:51:25 +0800

    adds mix chart in the dashboard with sample data

--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // chartjs
-import { Line } from 'react-chartjs-2';
+import { Line, Bar } from 'react-chartjs-2';
 
 // material ui
 import { makeStyles } from '@material-ui/core/styles';
@@ -28,7 +28,7 @@ const useStyles = makeStyles({
     },
 });
 
-const data = {
+const lineData = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
         {
@@ -55,7 +55,7 @@ const data = {
     ]
 };
 
-const options = {
+const lineOptions = {
     legend: {
         display: false,
     },
@@ -65,74 +65,171 @@ const options = {
     },
 };
 
+const mixData = {
+    // labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+        label: 'Sales',
+        type:'line',
+        data: [51, 65, 40, 49, 60, 37, 40],
+        fill: false,
+        borderColor: '#EC932F',
+        backgroundColor: '#EC932F',
+        pointBorderColor: '#EC932F',
+        pointBackgroundColor: '#EC932F',
+        pointHoverBackgroundColor: '#EC932F',
+        pointHoverBorderColor: '#EC932F',
+        yAxisID: 'y-axis-2'
+      },
+      {
+        type: 'bar',
+        label: 'Visitor',
+        data: [200, 185, 590, 621, 250, 400, 95],
+        fill: false,
+        backgroundColor: '#71B37C',
+        borderColor: '#71B37C',
+        hoverBackgroundColor: '#71B37C',
+        hoverBorderColor: '#71B37C',
+        yAxisID: 'y-axis-1'
+      }]
+  };
+  
+const mixOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    tooltips: {
+        mode: 'label'
+    },
+    elements: {
+        line: {
+            fill: false
+        }
+    },
+    scales: {
+        xAxes: [
+            {
+                display: true,
+                gridLines: {
+                    display: false
+                },
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            }
+        ],
+        yAxes: [
+            {
+                type: 'linear',
+                display: true,
+                position: 'left',
+                id: 'y-axis-1',
+                gridLines: {
+                    display: false
+                },
+                labels: {
+                    show: true
+                }
+            },
+            {
+                type: 'linear',
+                display: true,
+                position: 'right',
+                id: 'y-axis-2',
+                gridLines: {
+                    display: false
+                },
+                labels: {
+                    show: true
+                }
+            }
+        ]
+    }
+};
+  
+const plugins = [{
+    afterDraw: (chartInstance, easing) => {
+        const ctx = chartInstance.chart.ctx;
+        ctx.fillText("This text drawn by a plugin", 100, 100);
+    }
+}];
+
 function Dashboard() {
     const classes = useStyles();
 
     return (
-        <div className="row">
-            <div className="col-md-3">
-                <Card className={classes.root}>
-                    <CardContent>
-                        <Typography className={classes.title} color="textSecondary" gutterBottom>
-                            Card 1
-                        </Typography>
-                        <Line
-                            data={data}
-                            options={options}
-                        />
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
-                </Card>
+        <div className="dashboard">
+            <div className="cards-container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <Card className={classes.root}>
+                            <CardContent>
+                                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    Card 1
+                                </Typography>
+                                <Line
+                                    data={lineData}
+                                    options={lineOptions}
+                                />
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+                    <div className="col-md-3">
+                        <Card className={classes.root}>
+                            <CardContent>
+                                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    Card 2
+                                </Typography>
+                                <Line
+                                    data={lineData}
+                                    options={lineOptions}
+                                />
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+                    <div className="col-md-3">
+                        <Card className={classes.root}>
+                            <CardContent>
+                                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    Card 3
+                                </Typography>
+                                <Line
+                                    data={lineData}
+                                    options={lineOptions}
+                                />
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+                    <div className="col-md-3">
+                        <Card className={classes.root}>
+                            <CardContent>
+                                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    Card 4
+                                </Typography>
+                                <Line
+                                    data={lineData}
+                                    options={lineOptions}
+                                />
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+                </div>
             </div>
-            <div className="col-md-3">
-                <Card className={classes.root}>
-                    <CardContent>
-                        <Typography className={classes.title} color="textSecondary" gutterBottom>
-                            Card 2
-                        </Typography>
-                        <Line
-                            data={data}
-                            options={options}
-                        />
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
-                </Card>
-            </div>
-            <div className="col-md-3">
-                <Card className={classes.root}>
-                    <CardContent>
-                        <Typography className={classes.title} color="textSecondary" gutterBottom>
-                            Card 3
-                        </Typography>
-                        <Line
-                            data={data}
-                            options={options}
-                        />
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
-                </Card>
-            </div>
-            <div className="col-md-3">
-                <Card className={classes.root}>
-                    <CardContent>
-                        <Typography className={classes.title} color="textSecondary" gutterBottom>
-                            Card 4
-                        </Typography>
-                        <Line
-                            data={data}
-                            options={options}
-                        />
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
-                </Card>
+            <div className="mix-chart mt-4 border rounded shadow-sm p-3 mb-5 bg-white rounded p-2 bg-white">
+                <Bar
+                    data={mixData}
+                    options={mixOptions}
+                    plugins={plugins}
+                    height={350}
+                />
             </div>
         </div>
     );

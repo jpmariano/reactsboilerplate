8538866c14f79239a6262c656162040cb542e263
zeejay09 <zeejaybelamide@gmail.com>
2020-06-30 17:45:41 +0800

    adds conditional rendering to the PasswordResetContainer that contains both the PasswordResetPage and NewPasswordPage based on the URL

--- src/containers/PasswordResetContainer.js
+++ src/containers/PasswordResetContainer.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 // components
 import PasswordResetPage from '../components/Password/Reset';
+import NewPasswordPage from '../components/Password/New';
 
 function PasswordResetContainer() {
+
+    const currUrl = window.location.pathname;
+
     return (
-        <div className="password-reset-container">
-            <div className="content p-3">
-                <PasswordResetPage />
-            </div>
-        </div>
+        <>
+            {
+                currUrl === '/password-reset' ?
+                    <div className="password-reset-container">
+                        <div className="content p-3">
+                            <PasswordResetPage />
+                        </div>
+                    </div>
+                :
+                    currUrl === '/new-password' &&
+                        <div className="new-password-container">
+                            <div className="content p-3">
+                                <NewPasswordPage />
+                            </div>
+                        </div>
+            }
+        </>
     );
 }
 

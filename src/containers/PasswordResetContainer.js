d0af79f57447b248e49a54cd1eb20e3a28582fcb
zeejay09 <zeejaybelamide@gmail.com>
2020-07-01 07:58:44 +0800

    adds a alert renderer from the value of the alert reducer

--- src/containers/PasswordResetContainer.js
+++ src/containers/PasswordResetContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 // components
 import PasswordResetPage from '../components/Password/Reset';
@@ -7,6 +8,7 @@ import NewPasswordPage from '../components/Password/New';
 function PasswordResetContainer(props) {
 
     const currUrl = window.location.pathname;
+    const alert = useSelector(state => state.alert);
     const { match } = props;
     const vkey = match.params.vkey;
 
@@ -23,6 +25,10 @@ function PasswordResetContainer(props) {
                     vkey &&
                         <div className="new-password-container">
                             <div className="content p-3">
+                                {
+                                    alert.passwordResetMessage &&
+                                        <div className={`alert ${alert.type} m-3 text-center`}>{alert.passwordResetMessage}</div>
+                                }
                                 <NewPasswordPage vkey={vkey}/>
                             </div>
                         </div>

bc57d3bc4eca14a01fdba7c771fd9a53f4821878
zeejay09 <zeejaybelamide@gmail.com>
2020-07-01 14:19:36 +0800

    adds the success page into the password reset container

--- src/containers/PasswordResetContainer.js
+++ src/containers/PasswordResetContainer.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 // components
 import PasswordResetPage from '../components/Password/Reset';
 import NewPasswordPage from '../components/Password/New';
+import SuccessPage from '../components/Password/Success';
 
 function PasswordResetContainer(props) {
 
@@ -22,7 +23,7 @@ function PasswordResetContainer(props) {
                         </div>
                     </div>
                 :
-                    vkey &&
+                    vkey ?
                         <div className="new-password-container">
                             <div className="content p-3">
                                 {
@@ -30,6 +31,17 @@ function PasswordResetContainer(props) {
                                         <div className={`alert ${alert.type} m-3 text-center`}>{alert.passwordResetMessage}</div>
                                 }
                                 <NewPasswordPage vkey={vkey}/>
+                            </div>
+                        </div>
+                    :
+                        currUrl === '/reset-password/success' &&
+                        <div className="password-change-success-container">
+                            <div className="content p-3">
+                                {
+                                    alert.passwordResetMessage &&
+                                        <div className={`alert ${alert.type} m-3 text-center`}>{alert.passwordResetMessage}</div>
+                                }
+                                <SuccessPage />
                             </div>
                         </div>
             }

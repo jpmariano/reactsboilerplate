a06ac8a8d56574ff696f7432d008917aa3ad0316
zeejay09 <zeejaybelamide@gmail.com>
2020-07-03 15:00:50 +0800

    encloses the fields in a row

--- src/components/Common/Filter.js
+++ src/components/Common/Filter.js
@@ -21,48 +21,50 @@ function Filter(props) {
                 } = props;
                 return (
                     <form onSubmit={handleSubmit} className="form-inline">
-                        <div className="form-group mr-sm-2 mb-2">
-                            <label for="username">Username</label>
-                            <input
-                                type="text"
-                                className="form-control-plaintext border border-dark rounded"
-                                id="username"
-                                value={values.username}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
+                        <div className="row mr-0 ml-0 mb-2">
+                            <div className="form-group mr-sm-2 mb-2">
+                                <label for="username">Username</label>
+                                <input
+                                    type="text"
+                                    className="form-control-plaintext border border-dark rounded"
+                                    id="username"
+                                    value={values.username}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                            </div>
+                            <div className="form-group mx-sm-2 mb-2">
+                                <label for="inputPassword2">Status</label>
+                                <select className="form-control-plaintext border border-dark rounded">
+                                    <option value={-1}>&nbsp; — Any —</option>
+                                    <option value={1}>Active</option>
+                                    <option value={0}>Inactive</option>
+                                </select>
+                            </div>
+                            <div className="form-group mx-sm-2 mb-2">
+                                <label for="inputPassword2">Role</label>
+                                <select className="form-control-plaintext border border-dark rounded">
+                                    <option value={-1}>&nbsp; — Any —</option>
+                                    {
+                                        roles.map((role, index) => (
+                                            <option key={index} value={role.rid}>{role.name}</option>
+                                        ))
+                                    }
+                                </select>
+                            </div>
+                            <div className="form-group mx-sm-2 mb-2">
+                                <label for="inputPassword2">Permission</label>
+                                <select className="form-control-plaintext border border-dark rounded">
+                                    <option value={-1}>&nbsp; — Any —</option>
+                                    {
+                                        permissions.map((permission, index) => (
+                                            <option key={index} value={permission.pid}>{permission.name}</option>
+                                        ))
+                                    }
+                                </select>
+                            </div>
                         </div>
-                        <div className="form-group mx-sm-2 mb-2">
-                            <label for="inputPassword2">Status</label>
-                            <select className="form-control-plaintext border border-dark rounded">
-                                <option value={-1}>&nbsp; — Any —</option>
-                                <option value={1}>Active</option>
-                                <option value={0}>Inactive</option>
-                            </select>
-                        </div>
-                        <div className="form-group mx-sm-2 mb-2">
-                            <label for="inputPassword2">Role</label>
-                            <select className="form-control-plaintext border border-dark rounded">
-                                <option value={-1}>&nbsp; — Any —</option>
-                                {
-                                    roles.map((role, index) => (
-                                        <option key={index} value={role.rid}>{role.name}</option>
-                                    ))
-                                }
-                            </select>
-                        </div>
-                        <div className="form-group mx-sm-2 mb-2">
-                            <label for="inputPassword2">Permission</label>
-                            <select className="form-control-plaintext border border-dark rounded">
-                                <option value={-1}>&nbsp; — Any —</option>
-                                {
-                                    permissions.map((permission, index) => (
-                                        <option key={index} value={permission.pid}>{permission.name}</option>
-                                    ))
-                                }
-                            </select>
-                        </div>
-                        <button type="submit" className="btn btn-primary mb-2">Confirm identity</button>
+                        <button type="submit" className="d-block btn btn-primary">Apply</button>
                     </form>
                 );
             }}
